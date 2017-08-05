f47dbab8cec6aa200a92782ca0f030cb7da66b4c
Bruno da Silva João <vinagreti@users.noreply.github.com>
2017-08-04 19:30:15 -0600

    README.md and build

--- src/app/Helpers/Request.js
+++ src/app/Helpers/Request.js
@@ -1,3 +1,3 @@
 import fetch from '@ckldeveloper/fetch'
-//export default fetch.api('http://52.67.130.202:8000/api/v1/', {})
-export default fetch.api('http://localhost:8000/api/v1/', {})
+export default fetch.api('http://52.67.130.202:8000/api/v1/', {})
+//export default fetch.api('http://localhost:8000/api/v1/', {})
